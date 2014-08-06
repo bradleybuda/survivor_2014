d2dfca824f405e1668a366d732431fff28e77bc1
Bradley Buda <bradleybuda@gmail.com>
2014-08-06 08:23:16 -0700

    Rename from survivor_2014 to survivor

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Survivor.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :survivor_2014,
+    [app: :survivor,
      version: "0.0.1",
      elixir: "~> 0.15.0",
      deps: deps]
