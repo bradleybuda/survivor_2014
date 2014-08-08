b38c07e908df7760783d52cd92c0eda05a166fb6
Bradley Buda <bradleybuda@gmail.com>
2014-08-07 22:34:29 -0700

    Load teams and DVOA from file

--- lib/survivor/team.ex
+++ lib/survivor/team.ex
@@ -1,48 +1,15 @@
 defmodule Survivor.Team do
-  defstruct name: ""
+  defstruct name: "", dvoa: nil
 
   def load_all_from_disk do
-    all = [
-     %Survivor.Team{name: "CHI"},
-     %Survivor.Team{name: "DET"},
-     %Survivor.Team{name: "GB"},
-     %Survivor.Team{name: "MIN"},
+    csv = CSVLixir.read(File.read!("data/teams.csv"))
+    [_|data] = csv
 
-     %Survivor.Team{name: "ATL"},
-     %Survivor.Team{name: "CAR"},
-     %Survivor.Team{name: "NO"},
-     %Survivor.Team{name: "TB"},
-
-     %Survivor.Team{name: "DAL"},
-     %Survivor.Team{name: "NYG"},
-     %Survivor.Team{name: "PHI"},
-     %Survivor.Team{name: "WAS"},
-
-     %Survivor.Team{name: "ARI"},
-     %Survivor.Team{name: "SF"},
-     %Survivor.Team{name: "SEA"},
-     %Survivor.Team{name: "STL"},
-
-     %Survivor.Team{name: "BAL"},
-     %Survivor.Team{name: "CIN"},
-     %Survivor.Team{name: "CLE"},
-     %Survivor.Team{name: "PIT"},
-
-     %Survivor.Team{name: "HOU"},
-     %Survivor.Team{name: "IND"},
-     %Survivor.Team{name: "JAC"},
-     %Survivor.Team{name: "TEN"},
-
-     %Survivor.Team{name: "BUF"},
-     %Survivor.Team{name: "MIA"},
-     %Survivor.Team{name: "NE"},
-     %Survivor.Team{name: "NYJ"},
-
-     %Survivor.Team{name: "DEN"},
-     %Survivor.Team{name: "KC"},
-     %Survivor.Team{name: "OAK"},
-     %Survivor.Team{name: "SD"},
-    ]
+    all = Enum.map data, fn record ->
+      [name,dvoa_s] = record
+      {dvoa, _} = Float.parse(dvoa_s)
+      %Survivor.Team{name: name, dvoa: dvoa}
+    end
 
     Enum.group_by all, fn team -> team.name end
   end
