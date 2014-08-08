a17a3355d2ae5d8cfda9a87b5a39a0d7b1b3b6f4
Bradley Buda <bradleybuda@gmail.com>
2014-08-08 06:42:17 -0700

    Small refactor

--- lib/survivor/pick.ex
+++ lib/survivor/pick.ex
@@ -2,20 +2,23 @@ defmodule Survivor.Pick do
   defstruct game: nil, home_victory: false
 
   def winner(pick) do
-    case pick.home_victory do
-      true ->
-        pick.game.home_team
-      false ->
-        pick.game.away_team
-    end
+    [winner, _] = winner_and_loser pick
+    winner
   end
 
   def loser(pick) do
+    [_, loser] = winner_and_loser pick
+    loser
+  end
+
+  defp winner_and_loser(pick) do
+    game = pick.game
+    teams = [game.home_team, game.away_team]
     case pick.home_victory do
       true ->
-        pick.game.away_team
+        teams
       false ->
-        pick.game.home_team
+        Enum.reverse(teams)
     end
   end
 end
