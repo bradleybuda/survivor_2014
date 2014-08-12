fa4a154b906a72d86203321d80a4bb3a9a87a58d
Bradley Buda <bradleybuda@gmail.com>
2014-08-12 09:51:13 -0700

    Shorten tests with an import

--- test/survivor/pick_set_test.exs
+++ test/survivor/pick_set_test.exs
@@ -1,5 +1,6 @@
 defmodule Survivor.PickSetTest do
   use ExUnit.Case, async: true
+  import Survivor.PickSet
 
   test "probability that two independent picks happen together" do
     [week1|_] = Survivor.Schedule.load_from_disk(Survivor.Team.load_all_from_disk)
@@ -7,10 +8,10 @@ defmodule Survivor.PickSetTest do
     pick1 = %Survivor.Pick{game: game1, home_victory: true}
     pick2 = %Survivor.Pick{game: game2, home_victory: false}
 
-    pick_set = Enum.reduce [pick1, pick2], Survivor.PickSet.empty, &Survivor.PickSet.add(&2, &1)
+    pick_set = Enum.reduce [pick1, pick2], empty(), &add(&2, &1)
 
     p_both = Survivor.Pick.probability(pick1) * Survivor.Pick.probability(pick2)
-    assert p_both == Survivor.PickSet.probability(pick_set)
+    assert p_both == probability(pick_set)
   end
 
   test "probability that the same pick happens twice" do
@@ -19,9 +20,9 @@ defmodule Survivor.PickSetTest do
     pick1 = %Survivor.Pick{game: game1, home_victory: true}
     pick2 = %Survivor.Pick{game: game1, home_victory: true}
 
-    pick_set = Enum.reduce [pick1, pick2], Survivor.PickSet.empty, &Survivor.PickSet.add(&2, &1)
+    pick_set = Enum.reduce [pick1, pick2], empty(), &add(&2, &1)
 
-    assert Survivor.Pick.probability(pick1) == Survivor.PickSet.probability(pick_set)
+    assert Survivor.Pick.probability(pick1) == probability(pick_set)
   end
 
   test "probability that opposing picks both happen" do
@@ -30,13 +31,13 @@ defmodule Survivor.PickSetTest do
     pick1 = %Survivor.Pick{game: game1, home_victory: true}
     pick2 = Survivor.Pick.not(pick1)
 
-    pick_set = Enum.reduce [pick1, pick2], Survivor.PickSet.empty, &Survivor.PickSet.add(&2, &1)
+    pick_set = Enum.reduce [pick1, pick2], empty, &add(&2, &1)
 
-    assert 0.0 == Survivor.PickSet.probability(pick_set)
+    assert 0.0 == probability(pick_set)
   end
 
   test "empty pick set has equality" do
-    assert Survivor.PickSet.empty() == Survivor.PickSet.empty()
+    assert empty() == empty()
   end
 
   test "single-entry pick set has equality" do
@@ -44,15 +45,15 @@ defmodule Survivor.PickSetTest do
     [game1|_] = week1
     pick1 = %Survivor.Pick{game: game1, home_victory: true}
 
-    assert Survivor.PickSet.add(Survivor.PickSet.empty(), pick1) == Survivor.PickSet.add(Survivor.PickSet.empty(), pick1)
+    assert add(empty(), pick1) == add(empty(), pick1)
   end
 
   test "adding a pick to a set twice doesn't break it" do
     [week1|_] = Survivor.Schedule.load_from_disk(Survivor.Team.load_all_from_disk)
     [game1|_] = week1
     pick1 = %Survivor.Pick{game: game1, home_victory: true}
-    pick_set_1 = Survivor.PickSet.add(Survivor.PickSet.empty(), pick1)
-    pick_set_2 = Survivor.PickSet.add(pick_set_1, pick1)
+    pick_set_1 = add(empty(), pick1)
+    pick_set_2 = add(pick_set_1, pick1)
 
     assert pick_set_1 == pick_set_2
   end
@@ -63,8 +64,8 @@ defmodule Survivor.PickSetTest do
     pick1 = %Survivor.Pick{game: game1, home_victory: true}
     pick2 = %Survivor.Pick{game: game2, home_victory: false}
 
-    pick_set_1 = Enum.reduce [pick1, pick2], Survivor.PickSet.empty, &Survivor.PickSet.add(&2, &1)
-    pick_set_2 = Enum.reduce [pick1, pick2], Survivor.PickSet.empty, &Survivor.PickSet.add(&2, &1)
+    pick_set_1 = Enum.reduce [pick1, pick2], empty, &add(&2, &1)
+    pick_set_2 = Enum.reduce [pick1, pick2], empty, &add(&2, &1)
 
     assert pick_set_1 == pick_set_2
   end
@@ -75,8 +76,8 @@ defmodule Survivor.PickSetTest do
     pick1 = %Survivor.Pick{game: game1, home_victory: true}
     pick2 = %Survivor.Pick{game: game2, home_victory: false}
 
-    pick_set_1 = Enum.reduce [pick1, pick2], Survivor.PickSet.empty, &Survivor.PickSet.add(&2, &1)
-    pick_set_2 = Enum.reduce [pick2, pick1], Survivor.PickSet.empty, &Survivor.PickSet.add(&2, &1)
+    pick_set_1 = Enum.reduce [pick1, pick2], empty, &add(&2, &1)
+    pick_set_2 = Enum.reduce [pick2, pick1], empty, &add(&2, &1)
 
     assert pick_set_1 == pick_set_2
   end
