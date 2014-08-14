f87f3c5ae028ce3e7a81575184e6b4cd6bea0faf
Bradley Buda <bradleybuda@gmail.com>
2014-08-14 08:35:31 -0700

    Add second profiling method as timer

--- lib/survivor/profiler.ex
+++ lib/survivor/profiler.ex
@@ -1,16 +1,23 @@
 defmodule Survivor.Profiler do
   import ExProf.Macro
 
-  def run do
+  def profile do
     profile do
-      teams = Survivor.Team.load_all_from_disk
-      schedule = Survivor.Schedule.load_from_disk(teams)
-      all = Survivor.Entry.all(schedule)
-      week_strategies = Enum.at all, 3
-      listed = week_strategies |> Enum.to_list
-      IO.inspect "length: #{length(listed)}"
+      do_work
     end
-
     :done
+  end
+
+  def time do
+    :timer.tc(Survivor.Profiler, :do_work, [])
+  end
+
+  def do_work do
+    teams = Survivor.Team.load_all_from_disk
+    schedule = Survivor.Schedule.load_from_disk(teams)
+    all = Survivor.Entry.all(schedule)
+    week_strategies = Enum.at all, 3
+    listed = week_strategies |> Enum.to_list
+    IO.inspect "length: #{length(listed)}"
   end
 end
